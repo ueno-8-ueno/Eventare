8f0c8e95acccb04cf58859c86e2c751d3e3c5d51
EC2 Default User <ec2-user@ip-172-31-45-67.ap-northeast-1.compute.internal>
2023-08-15 06:21:06 +0000

    [Finish]イベント投稿機能および一覧, 詳細機能(レイアウトは未実装)

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,10 +1,28 @@
 class EventsController < ApplicationController
   def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+    @event.user_id = current_user.id
+    @event.save
+    redirect_to events_path
   end
 
   def index
+    @events = Event.all
   end
 
   def show
+    @event = Event.find(params[:id])
   end
+
+
+  private
+
+  def event_params
+    params.require(:event).permit(:name, :introduction, :genre, :start_at, :end_at)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get 'homes/about'
 
-  resources :events, only: [:new, :index, :show]
+  resources :events, only: [:new, :create, :index, :show]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
