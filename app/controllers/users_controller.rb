c5bfae72a289715a7ca45981eff581f6389e6893
EC2 Default User <ec2-user@ip-172-31-45-67.ap-northeast-1.compute.internal>
2023-08-16 07:56:02 +0000

    [Finish]プロフィールの編集機能

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,29 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user != current_user
+      redirect_to user_path(current_user.id)
+      return
+    end
+
+    if @user.update(user_params)
+      flash[:notice] = "プロフィールの保存が完了しました"
+      redirect_to user_path(current_user.id)
+    else
+      render :edit
+    end
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
   end
 end
