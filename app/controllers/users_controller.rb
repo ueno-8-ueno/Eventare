aea14ecee08f4106690d06e1f54ae6465150f168
EC2 Default User <ec2-user@ip-172-31-45-67.ap-northeast-1.compute.internal>
2023-08-23 04:53:25 +0000

    [Finish]ユーザ編集画面へのアクセス制御

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :is_matching_login_user, only: [:edit, :update]
+  
   def show
     @user = User.find(params[:id])
     @events = @user.events.page(params[:page]) # ページネーション
@@ -27,7 +29,16 @@ class UsersController < ApplicationController
 
   private
 
+  # ストロングパラメータ
   def user_params
     params.require(:user).permit(:name, :introduction, :profile_image)
+  end
+
+  # ユーザプロフィールへのアクセス制御
+  def is_matching_login_user
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to user_path(current_user)
+    end
   end
 end
